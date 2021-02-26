6bc49bd7080c482bc64d29e2271c410f0f336545
Syam Marcus <marcuss@coventry.ac.uk>
2021-02-26 21:48:48 +0000

    version 0.1.9
    (validation branch)
    Feat:
    - Added json scheme file for listings route
    - Added validation.js controller file to be used in routes as middleware
    - Added update listing functions to listings route
    - Added validation middleware when creating and updating listings
    - Installed "jsonscheme" npm package

--- routes/listings.js
+++ routes/listings.js
@@ -1,14 +1,16 @@
 const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser')
-const auth = require('../controllers/auth');
+//const auth = require('../controllers/auth');
 const model = require('../models/listings');
-const can = require('../permissions/users');
+//const can = require('../permissions/users');
+const {validateListing} = require('../controllers/validation');
 
 const router = Router({prefix: '/TCS/listings'});
 
 router.get('/', getAll);
 router.get('/:id([0-9]{1,})', getById); 
-router.post('/', bodyParser(), createListing);
+router.post('/', bodyParser(), validateListing, createListing);
+router.put('/:id([0-9]{1,})', bodyParser(), validateListing, updateListing);
 
 async function getAll(ctx) {
   let articles = await model.getAll();
@@ -35,6 +37,22 @@ async function createListing(ctx) {
   }
 }
 
+
+async function updateListing(ctx) {
+  const id = ctx.params.id;
+  let result = await model.getById(id);  // check it exists
+  if (result.length) {
+    let article = result[0];
+    // exclude fields that should not be updated
+    const {ID, dateCreated, dateModified, authorID, ...body} = ctx.request.body;
+    // overwrite updatable fields with remaining body data
+    Object.assign(article, body);
+    result = await model.update(article);
+    if (result.affectedRows) {
+      ctx.body = {ID: id, updated: true, link: ctx.request.path};
+    }
+  }
+}
 
 
 module.exports = router;
